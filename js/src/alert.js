0b5a71d44ca334c843f814039f42ed43c62d4faa
GeoSot <geo.sotis@gmail.com>
2021-06-02 16:58:30 +0300

    use a streamlined way to trigger component dismiss

--- js/src/alert.js
+++ js/src/alert.js
@@ -11,6 +11,7 @@ import {
 } from './util/index'
 import EventHandler from './dom/event-handler'
 import BaseComponent from './base-component'
+import { enableDismissTrigger } from './util/plugin-functions'
 
 /**
  * ------------------------------------------------------------------------
@@ -21,13 +22,9 @@ import BaseComponent from './base-component'
 const NAME = 'alert'
 const DATA_KEY = 'bs.alert'
 const EVENT_KEY = `.${DATA_KEY}`
-const DATA_API_KEY = '.data-api'
-
-const SELECTOR_DISMISS = '[data-bs-dismiss="alert"]'
 
 const EVENT_CLOSE = `close${EVENT_KEY}`
 const EVENT_CLOSED = `closed${EVENT_KEY}`
-const EVENT_CLICK_DATA_API = `click${EVENT_KEY}${DATA_API_KEY}`
 
 const CLASS_NAME_ALERT = 'alert'
 const CLASS_NAME_FADE = 'fade'
@@ -93,16 +90,6 @@ class Alert extends BaseComponent {
       }
     })
   }
-
-  static handleDismiss(alertInstance) {
-    return function (event) {
-      if (event) {
-        event.preventDefault()
-      }
-
-      alertInstance.close(this)
-    }
-  }
 }
 
 /**
@@ -111,8 +98,7 @@ class Alert extends BaseComponent {
  * ------------------------------------------------------------------------
  */
 
-EventHandler.on(document, EVENT_CLICK_DATA_API, SELECTOR_DISMISS, Alert.handleDismiss(new Alert()))
-
+enableDismissTrigger((instance, event) => instance.close(event.target), Alert)
 /**
  * ------------------------------------------------------------------------
  * jQuery
--- js/src/modal.js
+++ js/src/modal.js
@@ -19,6 +19,7 @@ import SelectorEngine from './dom/selector-engine'
 import { getWidth as getScrollBarWidth, hide as scrollBarHide, reset as scrollBarReset } from './util/scrollbar'
 import BaseComponent from './base-component'
 import Backdrop from './util/backdrop'
+import { enableDismissTrigger } from './util/plugin-functions'
 
 /**
  * ------------------------------------------------------------------------
@@ -65,7 +66,6 @@ const CLASS_NAME_STATIC = 'modal-static'
 const SELECTOR_DIALOG = '.modal-dialog'
 const SELECTOR_MODAL_BODY = '.modal-body'
 const SELECTOR_DATA_TOGGLE = '[data-bs-toggle="modal"]'
-const SELECTOR_DATA_DISMISS = '[data-bs-dismiss="modal"]'
 
 /**
  * ------------------------------------------------------------------------
@@ -128,8 +128,6 @@ class Modal extends BaseComponent {
 
     this._setEscapeEvent()
     this._setResizeEvent()
-
-    EventHandler.on(this._element, EVENT_CLICK_DISMISS, SELECTOR_DATA_DISMISS, event => this.hide(event))
 
     EventHandler.on(this._dialog, EVENT_MOUSEDOWN_DISMISS, () => {
       EventHandler.one(this._element, EVENT_MOUSEUP_DISMISS, event => {
@@ -434,6 +432,8 @@ EventHandler.on(document, EVENT_CLICK_DATA_API, SELECTOR_DATA_TOGGLE, function (
 
   data.toggle(this)
 })
+
+enableDismissTrigger((instance, event) => instance.hide(event), Modal)
 
 /**
  * ------------------------------------------------------------------------
